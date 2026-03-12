ccb465361ebe1aa737fa165af01f3010da0230f3
riyanfazrirahman <riyanfazrirahman@gmail.com>
2026-03-13 02:13:25 +0700

    dropzone img

--- frontend/src/pages/PageClassification.tsx
+++ frontend/src/pages/PageClassification.tsx
@@ -1,9 +1,31 @@
+import { useState } from "react";
 import PageBreadcrumb from "../components/common/PageBreadCrumb";
 import PageMeta from "../components/common/PageMeta";
 import PageWrapper from "../components/common/PageWrapper";
-import DropzoneComponent from "../components/form/form-elements/DropZone";
+import ImgDropZone from "../components/form/form-elements/ImgDropZone";
 
 export default function PageClassification() {
+  const [previewUrl, setPreviewUrl] = useState<string | null>(null);
+  const [selectedFile, setSelectedFile] = useState<File | null>(null);
+
+  const [formData, setFormData] = useState({
+    nama_bank: "",
+    url_img_bank_old: "",
+    aktif: 1,
+    img_bank: null as File | null,
+  });
+
+  const handleFileSelect = (file: File) => {
+    setSelectedFile(file);
+    setFormData((prev) => ({
+      ...prev,
+      img_bank: file,
+    }));
+
+    const url = URL.createObjectURL(file);
+    setPreviewUrl(url);
+  };
+
   return (
     <PageWrapper>
       <PageMeta
@@ -13,7 +35,11 @@ export default function PageClassification() {
       <PageBreadcrumb pageTitle="Form Elements" />
       <div className="grid grid-cols-1">
         <div className="space-y-6">
-          <DropzoneComponent />
+          {/* <DropzoneComponent /> */}
+          <ImgDropZone
+            onFileSelect={handleFileSelect}
+            currentImage={previewUrl || undefined}
+          />
         </div>
       </div>
     </PageWrapper>
